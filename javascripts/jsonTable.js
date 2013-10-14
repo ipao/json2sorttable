f9de8aa01a9ecbae60cc1819b7736cab53e70d0f
Kamonpop Jarujit <kjarujit@gmail.com>
2013-10-14 17:35:19 +0700

    Create jsonTable.js

---
+++ javascripts/jsonTable.js
@@ -0,0 +1,46 @@
+//jsonTable.js by Kamonpop Jarujit. Licensed under http://www.apache.org/licenses/LICENSE-2.0
+function jsonTable(json) {
+	this.json = json;
+	var order = {field: undefined, asc: 1};
+	var addTo = this.addTo = function(el) {
+		var len = json.length;
+		if(!len) return;
+		var html = '<table class="jsonTable"><thead><tr>';
+		for(var key in json[0]) {
+			html += '<td><a href="#" class="header">' + key + '</a>' +
+				(order.field == key ? (order.asc == 1 ? '^' : 'v') : '') +
+				'</td>';
+		}
+		html += '</tr></thead><tbody>';
+		for(var i = 0; i < len; i++) {
+			html += '<tr>';
+			for(var key in json[i]) {
+				html += '<td>' + json[i][key] + '</td>';
+			}
+			html += '</tr>';
+		}
+		el.innerHTML = html + '</tbody></table>';
+		var head = document.getElementsByClassName('header');
+		len = head.length;
+		for(var i = 0; i < len; i++) {
+			head[i].onclick = function() {
+				sortWith(this.innerHTML, el);
+			};
+		}
+	}
+	var sortWith = this.sortWith = function(field, el) {
+		if(order.field == field) order.asc *= -1;
+		else order = {field: field, asc: 1};
+		if(isNaN(json[0][field])) {
+			json.sort(function (a, b) { 
+				return (a[field].localeCompare(b[field]) || -1) * order.asc;
+			});
+		}
+		else {
+			json.sort(function (a, b) { 
+				return (Number(a[field]) - Number(b[field]) || -1) * order.asc;
+			});
+		}
+		addTo(el);
+	}
+}
